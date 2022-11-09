7ba826034a3aa3c07e3d1b4e96428d1f0e0e39b7
Alessandro Proglio <ale.proglio@gmail.com>
2022-11-09 19:48:39 +0100

    Create example.py
    a simple example of how the library works

---
+++ example.py
@@ -0,0 +1,41 @@
+from serialLG1800 import serialLG1800 as slg
+
+lgport = 'socket://192.168.0.8:3800'
+lg = None
+
+def initLG(self):
+    lg = slg.LG1800(lgport)
+    
+def fetchInputs(self):
+    if lg.connected == True:
+        # retrieve the status of external input used by the operator
+        lg.inputLevels()
+        # do something
+        
+def riconnectLG(self, event=None):
+    if lg.connected == False:
+        if lg.connect(self.lgport):
+            fetchInputs()
+
+def runCT(self):
+    s = {}
+    # [...] fetch settings from database
+    
+    return lg.runCT(s["ttCT_absolute"], 
+       self.s["ttCTcheckImax"], self.s["ttCTImin"],
+       self.s["ttCTImax"], self.s["ttCTInom"], self.s["ttCTItolpos"], 
+       self.s["ttCTItolneg"], self.s["forAutotest"])            
+
+initLG()
+
+# the test bench can allow the connection of two devices, but only one at a time is under test
+# so the operator can connect an appliance while the other is under test.
+# an output of the LG1800 is used to drive a contactor and switch from one product to the other
+lg.outputFunctional("toggleEXT")
+
+# disconnect every output
+lg.oF("OFF") # translates in serial command "*SET 000;006" disable outputs to contactors controlling L1 and L2 
+# disconnect any capacitor used during the functional tests
+lg.oF("0uf") # translates in serial command "*SET 112;000" disable all autputs connected to phase capacitors
+# run Continuity Test
+resultsCT = runCT()
